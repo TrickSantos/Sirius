3b93a9314699c5d739d59705eeddef03c20eb967
unknown <admin@DESKTOP-LFITEFE.med.local>
2018-01-23 11:46:37 -0300

    Primeiro Commit

---
+++ Sirius/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System;
+using Com.Hitomi.Cmlibrary;
+using Android.Graphics;
+
+namespace Sirius
+{
+    [Activity(Label = "Sirius", MainLauncher = true, Theme = "@style/Theme.AppCompat")]
+    public class MainActivity : AppCompatActivity, IOnMenuSelectedListener
+    {
+        String[] classesNome = { "Amigo", "Companheiro", "Pesquisador", "Pioneiro", "Excursionista", "Guia" };
+        public void OnMenuSelected(int index)
+        {
+            Toast.MakeText(this, $"Você selecionou {classesNome[index]}", ToastLength.Short).Show();
+        }
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var circleMenu = FindViewById<CircleMenu>(Resource.Id.circleMenu1);
+
+            circleMenu.SetMainMenu(Color.LightGray, Resource.Drawable.ic_geral, Resource.Drawable.ic_fechar)
+                                        .AddSubMenu(Color.ParseColor("#27408B"), Resource.Drawable.ic_amigo)
+                                        .AddSubMenu(Color.ParseColor("#B22222"), Resource.Drawable.ic_companheiro)
+                                        .AddSubMenu(Color.ParseColor("#2E8B57"), Resource.Drawable.ic_pesquisador)
+                                        .AddSubMenu(Color.ParseColor("#696969"), Resource.Drawable.ic_pioneiro)
+                                        .AddSubMenu(Color.ParseColor("#DA70D6"), Resource.Drawable.ic_excursionista)
+                                        .AddSubMenu(Color.ParseColor("#FFD700"), Resource.Drawable.ic_guia)
+                                        .SetOnMenuSelectedListener(this);
+        }
+    }
+}
+
---
+++ Sirius/Model/Questoes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Sirius.Model
+{
+    class Questoes
+    {
+        public int Id { get; set; }
+        public string Pergunta { get; set; }
+        public string RespostaA { get; set; }
+        public string RespostaB { get; set; }
+        public string RespostaC { get; set; }
+        public string RespostaD { get; set; }
+        public string RespostaCerta { get; set; }
+
+        public Questoes(int Id, string Pergunta, string AnswerA, string AnswerB, string AnswerC, string AnswerD, string CorrectAnswer)
+        {
+            this.Id = Id;
+            this.Pergunta = Pergunta;
+            this.RespostaA = RespostaA;
+            this.RespostaB = RespostaB;
+            this.RespostaC = RespostaC;
+            this.RespostaD = RespostaD;
+            this.RespostaCerta = RespostaCerta;
+        }
+    }
+}
